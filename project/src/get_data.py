41161d93f5b2969806421256ed3ddfa58481b59a
Ali Nasrallah <ali.nasrallah04@gmail.com>
2025-08-17 19:18:07 -0400

    Edited date range for yfinance data

--- project/src/get_data.py
+++ project/src/get_data.py
@@ -2,6 +2,6 @@ import yfinance as yf
 from utils import save_csv
 
 tickers = ["AAPL", "MSFT", "GOOG", "AMZN", "TSLA"]
-df_api = yf.download(tickers, start="2018-08-01", end="2023-08-01").reset_index()
+df_api = yf.download(tickers, start="2020-08-17", end="2025-08-17").reset_index()
 
 _ = save_csv(df_api, prefix='api', source='yfinance', symbol=tickers)
