d9ef953667afe7ec71e406f59fab815a396aed82
Ali Nasrallah <ali.nasrallah04@gmail.com>
2025-08-20 20:52:11 -0400

    Created notebook for data processing, saved parquet data

--- project/src/get_data.py
+++ project/src/get_data.py
@@ -1,7 +1,11 @@
 import yfinance as yf
-from utils import save_csv
+from utils import save_csv, save_parquet
 
 tickers = ["AAPL", "MSFT", "GOOG", "AMZN", "TSLA"]
-df_api = yf.download(tickers, start="2020-08-17", end="2025-08-17").reset_index()
+df_api = yf.download(tickers, start="2020-08-17", end="2025-08-17", auto_adjust=False).reset_index()
 
-_ = save_csv(df_api, prefix='api', source='yfinance', symbol=tickers)
+csv_path = save_csv(df_api, prefix='api', source='yfinance', symbol=tickers)
+
+parquet_path = save_parquet(df_api, prefix='api', source='yfinance', symbol=tickers)
+
+
--- project/src/utils.py
+++ project/src/utils.py
@@ -3,15 +3,24 @@ from dotenv import load_dotenv
 import os, datetime as dt
 from pathlib import Path
 
+
 load_dotenv()
 RAW = Path(os.getenv('DATA_DIR_RAW'))
+PROC = Path(os.getenv('DATA_DIR_PROCESSED'))
 
 def ts():
     return dt.datetime.now().strftime('%Y%m%d-%H%M%S')
 
 def save_csv(df: pd.DataFrame, prefix: str, **meta):
-    print(RAW)
     mid = '_'.join([f"{k}-{v}" for k,v in meta.items()])
     path = RAW / f"{prefix}_{mid}_{ts()}.csv"
     df.to_csv(path, index=False)
     return path
+
+def save_parquet(df: pd.DataFrame, prefix: str, **meta):
+    mid = '_'.join([f"{k}-{v}" for k,v in meta.items()])
+    path = PROC / f"{prefix}_{mid}_{ts()}.parquet"
+    df.to_parquet(path, index=False)
+    return path
+
+
